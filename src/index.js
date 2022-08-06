e7c0ba7a9a02c41f4b4e8258d78de06f6c712e74
Minha Kwon <namisoft@naver.com>
2022-08-06 16:14:47 +0900

    edit build setting

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 //adding my imports
-import Background from './views/background';
+import Background from "./views/background";
 import Navbar from "./components/navbar.js";
 import Footer from "./components/footer.js";
-import About from './views/about';
-import Projects from './views/projects';
-import ContactMe from './views/contact';
+import About from "./views/about";
+import Projects from "./views/projects";
+import ContactMe from "./views/contact";
 
 //import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/index.scss";
 
-
 //new import for react 18+ using createRoot
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<React.Fragment>
-		<Background />
-		<Navbar />	
-		<About />
-		<Projects />
-		<ContactMe/>
-		<Footer />
-	</React.Fragment>
+  <React.Fragment>
+    <Background />
+    <Navbar />
+    <About />
+    <Projects />
+    <ContactMe />
+    <Footer />
+  </React.Fragment>
 );
 
 // If you want your app to work offline and load faster, you can change

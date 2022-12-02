6005df9ffe58e5321cfa2888815cc7f77ea6cf1a
Minha Kwon <namisoft@naver.com>
2022-12-02 17:12:04 +0900

    email contact 추가

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
     <div id="footer" className="container-fluid text-center py-5">
-      <div className="container">
-        <p>© 2022 • MyPortfolio - MH</p>
+      <div className="container d-flex justify-content-around">
+        <div className="p-2">© 2022 • MyPortfolio - MH </div>
+        <div className="p-2">
+          {" "}
+          • Contact Me : <a href="mailto:minhappp@naver.com">email link</a>
+        </div>
       </div>
     </div>
   );
